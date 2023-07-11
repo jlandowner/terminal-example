92708534ba9cb3b0a11a2d58450867bc11ea7b85
Jinu <jlandowner8@gmail.com>
2023-07-12 06:17:54 +0900

    Update override.go

--- override.go
+++ override.go
@@ -9,7 +9,7 @@ func main() {
 	ticker := time.Tick(time.Millisecond * 100)
 	for i := 1; i <= 100; i++ {
 		<-ticker
-        // write at previous cursor
+		// write at previous cursor
 		fmt.Printf("\x0cOn %d", i)
 	}
 	fmt.Println("\nAll done")
