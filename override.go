a8f3e1a286e8c16e2a5f539ba9e257a8a9f23cd4
Jinu <jlandowner8@gmail.com>
2023-07-12 06:17:27 +0900

    Create override.go

---
+++ override.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.Tick(time.Millisecond * 100)
+	for i := 1; i <= 100; i++ {
+		<-ticker
+        // write at previous cursor
+		fmt.Printf("\x0cOn %d", i)
+	}
+	fmt.Println("\nAll done")
+}
